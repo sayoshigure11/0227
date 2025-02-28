5a9a638d56dd562977287cba9a01d76519ea7e68
sayoshigure11 <shun255105@outlook.jp>
2025-03-01 05:12:47 +0900

    とりあえず

--- app/api/rendering/route.ts
+++ app/api/rendering/route.ts
@@ -1,24 +1,24 @@
-import { prisma } from "@/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    const users = await prisma.user.findMany()
-    return NextResponse.json({dataList: users, status: 200})
-}
-
-
-// import { Redis } from '@upstash/redis'
+// import { prisma } from "@/prisma";
 // import { NextResponse } from "next/server";
 
-// export const redis = new Redis({
-//   url: process.env.UPSTASH_REDIS_REST_URL,
-//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
-// })
-
 // export async function GET() {
-//     const users = await redis.get('foo');
+//     const users = await prisma.user.findMany()
 //     return NextResponse.json({dataList: users, status: 200})
 // }
+
+
+import { Redis } from '@upstash/redis'
+import { NextResponse } from "next/server";
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
+export async function GET() {
+    const users = await redis.get('datalist');
+    return NextResponse.json({dataList: users, status: 200})
+}
 // export async function POST() {
 //     const users = await redis.get('foo');
 //     return NextResponse.json({dataList: users, status: 200})
