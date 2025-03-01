3f88f76bb7334b7578ae8b5f3352036aa5f906f3
sayoshigure11 <shun255105@outlook.jp>
2025-03-01 13:13:21 +0900

    trailingSlashを追加。next exportとdeployを追加。gh-pagesをインストール

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/0227',
-  output: "export"
+  output: "export",
+  trailingSlash: true
 };
 
 export default nextConfig;
